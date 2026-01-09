27f5825731e25df2006ce7ff41665754e4365f7e
Andres821340 <andres.saltos@istvidanueva.edu.ec>
2026-01-09 09:41:05 -0500

    Simplify Program.cs for Postman testing, remove Swagger UI

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using System.Reflection;
 using System.Text;
 using WebApi.Data;
 using WebApi.Middleware;
@@ -11,19 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Configuración de servicios
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-// Configuración de Swagger
-builder.Services.AddSwaggerGen(options =>
-{
-    // Incluir documentación XML
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
-    if (File.Exists(xmlPath))
-    {
-        options.IncludeXmlComments(xmlPath);
-    }
-});
 
 // Configuración de DbContext con SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -53,22 +39,10 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configuración de middleware
+// Middleware
 app.UseRequestLogging();
 app.UseExceptionHandling();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1");
-        options.RoutePrefix = "swagger";
-        options.DocumentTitle = "WebApi - Documentación";
-    });
-}
-
-app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
